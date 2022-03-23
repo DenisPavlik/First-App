31a7461646f6ff0f6d2e61818c5a3b0c03ceeb11
Denys <denispavlik9@gmail.com>
2022-03-23 15:22:15 +0200

    06 added Navlinks and Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,23 @@
-import s from './Dialogs.module.css';
-
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogs__item}>
+        <div className={s.dialog}>Masha</div>
+        <div className={s.dialog}>Artem</div>
+        <div className={s.dialog}>Lena</div>
+        <div className={s.dialog}>Oleg</div>
+        <div className={s.dialog}>Sveta</div>
+        <div className={s.dialog}>Denys</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How is you IT course?</div>
+        <div className={s.message}>Yo</div>
+      </div>
     </div>
   );
 };
-
 
 export default Dialogs;
